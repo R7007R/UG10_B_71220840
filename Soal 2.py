b73be5683dd582efb4ad06593af937108c8bbac5
R7007R <117884377+R7007R@users.noreply.github.com>
2022-11-24 13:14:43 +0700

    Delete Soal 2.py

--- Soal 2.py
+++
@@ -1,7 +0,0 @@
-print ("========== PROGRAM PENGHITUNG PYTHAGORAS ==========")
-
-bulat1 = int(input(masukkan bilangan bulat pertama: ")
-bulat2 = int(input(masukkan bilangan bulat kedua: ")
-bulat3 = int(input(masukkan bilangan bulat ketiga: ")
-
-if 
