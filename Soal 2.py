798d739abed8fcb063d7e576ff0fbd5daaabbe38
R7007R <117884377+R7007R@users.noreply.github.com>
2022-11-24 13:15:20 +0700

    Add files via upload

---
+++ Soal 2.py
@@ -0,0 +1,10 @@
+print ("========== PROGRAM PENGHITUNG PYTHAGORAS ==========")
+
+bulat1 = int(input("masukkan bilangan bulat pertama: "))
+bulat2 = int(input("masukkan bilangan bulat kedua: "))
+bulat3 = int(input("masukkan bilangan bulat ketiga: "))
+
+if ((bulat1**2)+(bulat2**2) == (bulat3**2)):
+    print ("Merupakan Pythagoras")
+else:
+    print ("Bukan Merupakan Pythagoras")
